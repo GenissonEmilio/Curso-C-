0c7204e3fe0f43374ef9f6c2cd992e5f240e9cfa
Genisson Emilio <esgenisson@gmail.com>
2025-09-03 13:05:35 -0300

    fix something

---
+++ aulas/aula 03/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 10, count = 0;
+
+
+for (int i = 1; i <= num; i++)
+{
+    if (num % i == 0)
+    {
+        count++;
+    }
+}
+
+if (count == 2)
+{
+    Console.WriteLine($"{num} é um número primo.");
+}
+else
+{
+    Console.WriteLine($"{num} não é um número primo.");
+}
