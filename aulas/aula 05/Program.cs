865091efcc926a09439d252f9d175df0f58edc5e
Genisson Emilio <esgenisson@gmail.com>
2025-09-08 20:07:02 -0300

    class 05

---
+++ aulas/aula 05/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Digite um número: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for (int i = 1; i <= num; i++)
+{
+    if (num < 1)
+    {
+        Console.WriteLine("Número inválido. Digite um número maior que 0.");
+        return;
+    }
+    
+    if (num % i == 0)
+    {
+        count++;
+    }
+}
+
+if (count == 2)
+{
+    Console.WriteLine("O número é primo.");
+}
+else
+{
+    Console.WriteLine("O número não é primo.");
+}
